9d0983ba4030c13d38064b77495e4319da85b23d
yadnesh-19 <yadneshmore2006@gmail.com>
2025-08-09 08:19:04 +0530

    Update client.py

--- AI chatbot/client.py
+++ AI chatbot/client.py
@@ -5,10 +5,11 @@ openai.api_key = openai.openAI(api_key=" ")
 response = openai.ChatCompletion.create(
   model="gemini-2.5-flash",
   messages=[
-        {"role": "user", "content":"You are a person name harry who speaks hindi as  well as"
+        {"role": "user", "content":"You are a person name Yadnesh who speaks hindi as  well as"
          "English and is a coder" "Hello!",}
     ]
 )
 
 print(response)
+
 
